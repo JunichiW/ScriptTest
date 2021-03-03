6dc7da93a01754c918244500d744fc13cf7552bd
JunichiW <78809500+JunichiW@users.noreply.github.com>
2021-03-04 00:20:38 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        //配列を初期化する
+        int[] array = { 1,2,3,4,5 };
+
+        //配列の各要素の値を順番に表示する
+        for (int i = 0; i < array.Length; i++)
+        {
+            //配列の要素を表示する
+            Debug.Log(array[i]);
+
+
+        }
+
+        //配列の各要素を逆順に表示する
+        for (int a = array.Length - 1; 0 <= a; a--)
+
+        {
+            
+          
+          //配列の要素を表示する
+          Debug.Log(array[a]);
+
+            }
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
